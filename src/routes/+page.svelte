7851136a880d874898ac0ac2c5e471e37cd34528
Piergiorgio Gonni <pg@sekei.ca>
2023-10-23 17:05:57 -0400

    More mobile fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -76,11 +76,11 @@
 <svelte:window on:keydown={handleKeyDown} />
 
 <div
-	class="w-screen h-screen flex items-end lg:items-center justify-center pt-10 lg:p-10 cursor-default"
+	class="w-screen h-screen flex items-end lg:items-center justify-center pt-10 lg:p-10 cursor-default overflow-hidden"
 >
 	<img src={background} alt="" class="absolute inset-0 w-full h-full object-cover -z-10" />
 	<div
-		class="bg-white flex flex-col overflow-hidden backdrop-brightness-150 bg-opacity-80 shadow-window backdrop-blur-xl rounded-window h-full w-full max-h-[90%] lg:max-h-window max-w-window"
+		class="bg-white flex flex-col overflow-hidden backdrop-brightness-150 bg-opacity-80 shadow-window backdrop-blur-xl rounded-window h-full w-full max-h-[90%] lg:max-h-window lg:max-w-window"
 	>
 		<Input isFocused={isSearchFocused} />
 		<div class="py-2 overflow-y-auto flex-grow" bind:this={scrollContainer}>
