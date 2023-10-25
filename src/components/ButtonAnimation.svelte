97eb34e6e2de9a090d5abb10093734f3cc7f2813
Piergiorgio Gonni <pg@sekei.ca>
2023-10-25 15:32:07 -0400

    Button componenet

---
+++ src/components/ButtonAnimation.svelte
@@ -0,0 +1,42 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let pressCommand = false;
+	let pressSpace = false;
+
+	onMount(() => {
+		setTimeout(() => {
+			pressCommand = true;
+		}, 300);
+		setTimeout(() => {
+			pressSpace = true;
+		}, 500);
+	});
+</script>
+
+<div class="absolute w-full h-full -z-10 flex items-center justify-center gap-4 text-sm">
+	<div
+		class="{pressCommand
+			? ''
+			: 'extrude'} bg-[#351518] transition-all ease-button duration-200 border border-[#65282e] rounded-lg w-14 h-11 flex items-center justify-center"
+	>
+		<span class="bg-text-gradient text-transparent bg-clip-text">⌘</span>
+	</div>
+	<div
+		class="{pressSpace
+			? ''
+			: 'extrude'} bg-[#351518] transition-all ease-button duration-200 border border-[#65282e] rounded-lg w-40 h-11 flex items-center justify-center"
+	>
+		<span class="bg-text-gradient text-transparent bg-clip-text">Space</span>
+	</div>
+</div>
+
+<style>
+	.extrude {
+		box-shadow: 0px 6px #65282e;
+		transform: translate3d(0px, -6px, 0);
+	}
+	.ease-button {
+		transition: all 400 cubic-bezier(0.22, 0.12, 0.02, 1.26);
+	}
+</style>
